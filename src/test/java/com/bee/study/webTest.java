f3c3c978fdc72dbc36b7213461d5ac13295044fd
liangbe <9169lucifer@163.com>
2017-04-12 17:40:43 +0800

    My false

--- src/test/java/com/bee/study/webTest.java
+++ src/test/java/com/bee/study/webTest.java
@@ -12,6 +12,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Created by liangbe on 2017/4/12.
  */
 @RunWith(SpringRunner.class)
+/**写成这样要
+ * package :application 跟test的package名一样(同一级)
+ *
+ */
 @SpringBootTest
 public class webTest {
 
