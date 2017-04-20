b12d5042788e8d02f65cff3c7ef82fe58452e18a
liangbe <9169lucifer@163.com>
2017-04-20 11:11:11 +0800

    thymeleaf?

--- src/main/java/com/bee/study/controller/view/UserController.java
+++ src/main/java/com/bee/study/controller/view/UserController.java
@@ -1,9 +1,11 @@
 package com.bee.study.controller.view;
 
+import com.bee.study.entity.User;
+import com.bee.study.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by liangbe on 2017/4/18.
@@ -12,8 +14,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        return "index";
+    @Autowired
+    private  UserService userService;
+
+    @RequestMapping(value = "/hello")
+    public String  hello(Model model) {
+        model.addAttribute("name", userService.findOne(1L).getName());
+        return  "index";
     }
 }
